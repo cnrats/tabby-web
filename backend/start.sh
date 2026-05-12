cff08cb5dbc8fa1b62d661e81048a1116015809b
Caner Ateş <49502924+cnrats@users.noreply.github.com>
2026-05-13 02:09:22 +0300

    port binding

--- backend/start.sh
+++ backend/start.sh
@@ -2,6 +2,8 @@
 if [[ -n "$DOCKERIZE_ARGS" ]]; then
     dockerize $DOCKERIZE_ARGS
 fi
+
 cd /app
 /venv/*/bin/python ./manage.py migrate
-exec /venv/*/bin/gunicorn
+/venv/*/bin/python ./manage.py collectstatic --noinput
+exec /venv/*/bin/gunicorn --bind 0.0.0.0:8000 --workers 4 --timeout 120 tabby.wsgi:application
